fac3d26f51967cd806be44cf1c780ecdf7343f39
Ameer <ameerbsc2630@gmail.com>
2025-03-09 17:11:37 +0530

    refactor Brands component to replace Swiper with marquee for improved performance and layout

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -1,9 +1,4 @@
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/autoplay";
-
 import imgOne from "../assets/marvice/Brand Logos/img-1.png";
 import imgTwo from "../assets/marvice/Brand Logos/img-2.png";
 import imgThree from "../assets/marvice/Brand Logos/img-3.png";
@@ -11,41 +6,28 @@ import imgFour from "../assets/marvice/Brand Logos/img-4.png";
 import imgFive from "../assets/marvice/Brand Logos/img-5.png";
 import imgSix from "../assets/marvice/Brand Logos/img-6.png";
 
-const logos = [
-  { id: 1, img: imgOne },
-  { id: 2, img: imgTwo },
-  { id: 3, img: imgThree },
-  { id: 4, img: imgFour },
-  { id: 5, img: imgFive },
-  { id: 6, img: imgSix },
-];
+const logos = [imgOne, imgTwo, imgThree, imgFour, imgFive, imgSix];
 
 const Brands = () => {
   return (
-    <div className="py-10 text-center px-10">
-      <h1 className="text-[2.5rem] text-[#162340] font-semibold ">
+    <div className="py-10 text-center px-10 overflow-hidden container mx-auto">
+      <h1 className="text-[2.5rem] text-[#162340] font-semibold">
         Trusted by Leading Brands & Institutions
       </h1>
 
-      {/* Swiper Carousel */}
-      <Swiper
-        breakpoints={{
-          640: { slidesPerView: 3 }, // On small screens, show 3 logos
-          768: { slidesPerView: 4 }, // On tablets, show 4 logos
-          1024: { slidesPerView: 4}, // On desktops, show 6 logos
-        }}
-        spaceBetween={10}
-        autoplay={{ delay: 100, disableOnInteraction: false }}
-        loop={true}
-        modules={[Autoplay]}
-        className="mt-10 container mx-auto"
-      >
-        {logos.map((data) => (
-          <SwiperSlide key={data.id} className="flex flex-row justify-center">
-            <img src={data.img} alt="brand" className="h-[140px] w-[200px] object-fit" />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      {/* Marquee Wrapper */}
+      <div className="relative w-full mt-10 overflow-hidden">
+        <div className="flex whitespace-nowrap animate-marquee">
+          {[...logos, ...logos].map((img, index) => (
+            <img
+              key={index}
+              src={img}
+              alt="brand"
+              className="h-[100px] w-auto object-contain mx-10"
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
